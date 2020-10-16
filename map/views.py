6bcf82efbe3d5e5eb3bf562da2e44f069009c9b2
Neli Davtyan <nd2673@columbia.edu>
2020-10-16 00:27:41 +0000

    Adding map

---
+++ map/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from squirrels.models import Squirrel
+
+# Create your views here.
+
+def map(request, *args, **kwargs):
+	sightings = Squirrel.objects.all()[:50]
+	context={
+        'sightings':sightings
+	}
+	return render(request, 'map/map.html',context)
